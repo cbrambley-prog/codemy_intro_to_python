fe6317a2379be5345273380115d25a39010a193a
Catherine Rudie <mac2@Catherines-MacBook-Pro.local>
2025-08-09 01:28:31 +0100

    Adding the first two scripts created for this course

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+# prints "Hello World@" to the console
+print("Hello World!") # also adding a comment here because I can
+
+'''
+Adding a big multi-line comment
+Hello!
+'''
+
+print("I'm going to try printing a multi-line string here...\nIs this multi-line? Let's find out.\nIt is!") # trying a multi-line string
---
+++ maths_in_python.py
@@ -0,0 +1,20 @@
+
+# This script experiements with math operators
+
+print(3+2) # addition
+
+print(100-88) # subraction
+
+print(9*9) # multiplications
+
+print (9**2) # exponent
+
+print(10/5) # division, this outputs a float
+
+print(10//5) # this is called floor division and outputs an integer
+
+print(10%3) # modulus... I don't know what to expect from this
+
+# Now we're going to output calculations as part of  a string
+
+print(f"This is an f string, we're going to embed 2 squared, which will output 2**2 = {2**2}") # an f string
