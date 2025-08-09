479ef442963221601d5ced28c72cb90b3a8f859d
Catherine Rudie <mac2@Catherines-MacBook-Pro.local>
2025-08-09 12:41:49 +0100

    Adding a script that experiments with basic operatros and comparison operators

--- maths_in_python.py
+++ maths_in_python.py
@@ -1,5 +1,9 @@
 
-# This script experiements with math operators
+'''
+This script experiements with maths in Python.
+
+The first section covers basic operators
+'''
 
 print(3+2) # addition
 
@@ -18,3 +22,19 @@ print(10%3) # modulus... I don't know what to expect from this
 # Now we're going to output calculations as part of  a string
 
 print(f"This is an f string, we're going to embed 2 squared, which will output 2**2 = {2**2}") # an f string
+
+'''
+The next section covers comparison operators
+'''
+
+print(f"Does 2 equal 5? Answer: {2==5}") # equal to
+
+print(f"Does 3 not equal 5? Answer: {3!=5}") # not equal to
+
+print(f"Is 3 great than 3? Answer: {3>3}") # greater than
+
+print(f"Is 3 greater than to equal to 3? Answer: {3>=3}") # greater than to equal to
+
+print(f"Is 98 less than 110? Answer: {98<110}") # less than
+
+print(f"Is 98 less than to equal to 110? Answer: {98<=110}") # less than to equal to
