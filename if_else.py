f2b6d26ad3d531590b9d07fdd67064c52496640d
Catherine Rudie <mac2@Catherines-MacBook-Pro.local>
2025-08-09 23:05:34 +0100

    Cleaning up the output and making sure names are capitalised

--- if_else.py
+++ if_else.py
@@ -6,25 +6,31 @@ The syntax is if <condition1>: <code1> elif <condition2>: <code2> else: <code3>
 '''
 
 # A guessing game for a number between 1 and 10
-# Input the users name
-name = input("What's your name? ")
 
+from os import system
+
+# Input the users name
+system("clear")
+name = input("What's your name? ").capitalize() # if the name isn't inputted with a capital letter, change that
+
+system("clear")
 if name == "Chris" or name == "Rudie":
-    guess = int(input(f"\nThanks for having me over {name}! I'm thinking of a number between 1 and 10, guess what it is. You get 5 guesses!\n\n"))
+    guess = int(input(f"Thanks for having me over {name}! I'm thinking of a number between 1 and 10, guess what it is. You get 5 guesses!\n\n"))
 else:
-    guess = int(input(f"\nWhat's up {name}. I'm thinking of a number between 1 and 10, guess what it is. You get 5 guesses!\n\n"))
+    guess = int(input(f"What's up {name}. I'm thinking of a number between 1 and 10, guess what it is. You get 5 guesses!\n\n"))
 
 number = 4 # set the number we're trying to guess
 guesses = 0 # define the guess counter
 
 while guesses < 5: 
 
+    system("clear")
     if guess == number:
-        print(f"\nNice one {name}! You guessed right :)\n")
+        print(f"Nice one {name}! You guessed right :)\n")
         break # If correct, exit the loop
     elif abs(guess - number) == 1:
-        print("\nVery close!")
+        print("Very close!")
     else:
-        print("\nNope way off. Who are you again? ")
+        print("Nope way off. Who are you again? ")
     guess = int(input("Have another guess: "))
     guesses += 1 # For each guess, add 1 to the counter
